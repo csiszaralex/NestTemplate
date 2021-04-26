2ca0f5cf1fb8b08113b308abe3262efbb1a33b59
Alex <csiszaralex@gmail.com>
2021-04-26 23:35:28 +0200

    :memo: Adds readme

--- src/users/decorators/get-name.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../entities/user.entity';
-
-export const GetName = createParamDecorator((_, ctx: ExecutionContext): string => {
-  const req = ctx.switchToHttp().getRequest();
-  const user: User = req.user;
-
-  return user.name;
-});
